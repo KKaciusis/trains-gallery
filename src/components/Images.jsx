e3d60f8b2f566cb60dbafc535c9a1c3005ffa7fa
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-09 09:34:38 +0300

    images added

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class Images extends Component{
     render(){
         return(
-            <h3>this is from images.jsx</h3>
+            <div className="wrapper">
+                <h3>this is from images.jsx</h3>
+            </div>
         );
 
     }
