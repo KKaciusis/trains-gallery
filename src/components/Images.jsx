24664c443f18bde5f501385227be82136edd0662
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-09 08:33:37 +0300

    initial commit

---
+++ src/components/Images.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Images extends Component{
+    render(){
+        return(
+            <h3>this is from images.jsx</h3>
+        );
+
+    }
+}
+
+export default Images;
